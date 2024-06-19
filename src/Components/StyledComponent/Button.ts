85376aac6eb44135fe9fcade9ff69d99c6969aff
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-06-18 23:52:56 -0300

    change styles file

--- src/Components/StyledComponent/Button.ts
+++ src/Components/StyledComponent/Button.ts
@@ -4,4 +4,9 @@ export const Button = styled.button`
   background-color: aquamarine;
   padding: 0.5rem;
   border-radius: 0.5rem;
+  cursor: pointer;
+  border: none;
+  &:hover {
+    background-color: aliceblue;
+  }
 `;
